65034f4e0d5c5632b488291bb50a419ea7e97a5c
Carlos G <cagonzalez411@gmail.com>
2021-12-05 23:06:40 -0500

    Added eventListeners for buttons

--- game.js
+++ game.js
@@ -1,64 +1,72 @@
-let computerScore = 0
-let playerScore = 0
-let playerSelection
-let computerSelection
+let computerScore = 0;
+let playerScore = 0;
 
 function computerPlay() {
+
     let computerRandom = Math.floor(Math.random() * 3);
     if (computerRandom === 0) {
-        return "rock"
+        return "rock";
     } else if (computerRandom == 1) {
         return "paper";
     } else {
         return "scissors";
     }   
+    
 }    
 
+function playRound(playerSelection) {
 
-
-
-function playRound (playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
+    if (playerSelection == computerPlay()) {
         return "It's a tie!";
-    } else if ((playerSelection == "rock") && (computerSelection == "scissors")) {
+    } else if ((playerSelection == "rock") && (computerPlay() == "scissors")) {
         return "You win!";
-    } else if ((playerSelection == "paper") && (computerSelection == "rock")) {
+    } else if ((playerSelection == "paper") && (computerPlay() == "rock")) {
         return "You win!";
-    } else if ((playerSelection == "scissors") && (computerSelection == "paper")) {
+    } else if ((playerSelection == "scissors") && (computerPlay() == "paper")) {
         return "You win!";
     } else {
         return "You lose!";
     }
+
 }
 
-function adjustscore() {
-    if (playRound(playerSelection, computerSelection) == ("You win!")) {
-        playerScore += 1
-    } else if (playRound(playerSelection, computerSelection) == "You lose!") {
-        computerScore += 1
-    }
-}
-
-function game() {
-    // computerSelection = computerPlay()
-    // playerSelection = prompt("Pick rock, paper, or scissors").toLowerCase()
-    // console.log(playerSelection)
-    // adjustscore()
-    // console.log(playerScore)
-    // console.log(computerScore)
-
-    
+function determine_winner() {
 
     if (computerScore > playerScore) {
-        console.log("You lose the game!")
+        console.log("You lose the game!");
     } else if (computerScore < playerScore) {
-        console.log("You win the game!")
+        console.log("You win the game!");
     } else {
-        console.log("It was a tie!")
+        console.log("It was a tie!");
     }
 
 }
 
+function adjustScore() {
 
-console.log(game());
     
+
+}
+
+
+// function game() {
+
+//     let gamesPlayed = 1;
+//     while (gamesPlayed <= 5) {
+//         console.log(playRound(playerSelection));
+//         console.log(gamesPlayed)
+//         gamesPlayed += 1;
+//     }
+
+// }
+
+const rockbtn = document.querySelector('#rock');
+const paperbtn = document.querySelector('#paper');
+const scissorsbtn = document.querySelector('#scissors');
+
+rockbtn.addEventListener('click', () => console.log(playRound('rock')));
+paperbtn.addEventListener('click', () => console.log(playRound('paper')));
+scissorsbtn.addEventListener('click', () => console.log(playRound('scissors')));
+
+
+
